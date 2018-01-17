6d80496bd981fde189d93a1126ee605e4753b3dd
Afrish Khan S <ryanafrish7@gmail.com>
2018-01-17 18:50:02 +0530

    Fix animations for section

--- scripts/web-experiments-page.js
+++ scripts/web-experiments-page.js
@@ -11,18 +11,27 @@ navElem.animate([
     navElem.removeAttribute("faded");
 };
 
-let sectionCount = 0;
+const sectionObserver = new IntersectionObserver(entries => {
+    let count = 0;
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            count++;
+            entry.target.animate([
+                {opacity: 0, transform: "translateY(200px)"},
+                {opacity: 1, transform: "translateY(0)"}
+            ], {
+                duration: 400,
+                delay: count * 200,
+                easing: "cubic-bezier(0.39, 0.575, 0.565, 1)"
+            }).onfinish = () => {
+                entry.target.removeAttribute("faded");
+            };
+            sectionObserver.unobserve(entry.target);
+        }
+    });
+});
+
 document.body.querySelectorAll(".space > section").forEach(section => {
     section.setAttribute("faded", true);
-    section.animate([
-        {opacity: 0, transform: "translateY(200px)"},
-        {opacity: 1, transform: "translateY(0)"}
-    ], {
-        duration: 400,
-        delay: sectionCount * 200,
-        easing: "cubic-bezier(0.39, 0.575, 0.565, 1)"
-    }).onfinish = () => {
-        section.removeAttribute("faded");
-    };
-    sectionCount++;
+    sectionObserver.observe(section);
 });
